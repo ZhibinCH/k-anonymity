4ef65e7b552f880c77594074d80025a05ae1345a
Cheng, Zhibin <sebastianbingo@gmail.com>
2021-11-12 09:02:51 +0100

    set up the basic workflow of using arx

---
+++ src/main/java/challenge/KAnonymityExample.java
@@ -0,0 +1,14 @@
+package challenge;
+
+public class KAnonymityExample {
+	public static void main(String[] args) throws Exception {
+	// 1. Load data
+	// 2. Generalization
+	// 3. Define privacy models and transformation rules
+	// 4. Execute the anonymization algorithm
+	// 5. Access and compare data
+	// 6. Analyze re-identification risks
+	// 7. Store data	
+	System.out.println("Well done!");
+	}
+}
